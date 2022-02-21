6e0c00d1c159f79816ab97fdf175ee30f88cd155
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-02-21 12:41:51 +0200

    add BlendMode in DrawParams.h

--- utils/drawing/DrawParams.h
+++ utils/drawing/DrawParams.h
@@ -4,6 +4,16 @@
 #include "Point.h"
 
 inline constexpr auto UNKNOWN_RSRC_ID = -1;
+inline constexpr auto FULL_OPACITY = 255;
+inline constexpr auto ZERO_OPACITY = 0;
+
+enum class BlendMode : uint8_t
+{
+	NONE = 0, //value for SDL_BLENDMODE_NONE
+	BLEND = 1, //value for SDL_BLENDMODE_BLEND
+	ADD = 2, //value for SDL_BLENDMODE_ADD
+	MOD = 4  //value for SDL_BLENDMODE_MODE
+};
 
 struct DrawParams
 {
@@ -11,6 +21,8 @@ struct DrawParams
 
 	int32_t w = 0;
 	int32_t h = 0;
+
+	int32_t opacity = FULL_OPACITY;
 
 	int32_t rsrcId = UNKNOWN_RSRC_ID;
 };
