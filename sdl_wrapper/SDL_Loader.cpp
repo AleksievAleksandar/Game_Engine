0243b3d4c61964ba21a948bf34bf6817ec643712
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-03-03 16:37:27 +0200

    add TTF_Init(), IMG_Init() and Mix_OpenAudio()

--- sdl_wrapper/SDL_Loader.cpp
+++ sdl_wrapper/SDL_Loader.cpp
@@ -5,11 +5,12 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 
 int32_t SDL_Loader::init()
 {
 	//Initialize SDL
-	if (EXIT_SUCCESS != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER))
+	if (EXIT_SUCCESS != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO))
 	{
 		std::cerr << "ERROR -> SDL could not initialize. SDL_Error: " << SDL_GetError() << std::endl;
 		return EXIT_FAILURE;
@@ -17,13 +18,23 @@ int32_t SDL_Loader::init()
 
 	if (EXIT_SUCCESS != TTF_Init())
 	{
-		std::cerr << "TTF_Init() failed. Reason: " << SDL_GetError() << std::endl;
+		std::cerr << "TTF_Init() failed. Reason: " << TTF_GetError() << std::endl;
 		return EXIT_FAILURE;
 	}
 
 	constexpr int32_t imgFlags = IMG_INIT_PNG;
-	if (!(IMG_Init(imgFlags) & imgFlags)) {
+	if (!(IMG_Init(imgFlags) & imgFlags)) 
+	{
 		std::cerr << "SDL_image could not be initialised! SDL_image Error: " << IMG_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	if (0 > Mix_OpenAudio(44100,    //sound frequency
+		MIX_DEFAULT_FORMAT,         //sample format
+		2,                          //stereo hardware channels
+		2048))                      //chunk size
+	{
+		std::cerr << "SDL_mixer could not be initialised! SDL Error: " << Mix_GetError() << std::endl;
 		return EXIT_FAILURE;
 	}
 
@@ -34,5 +45,6 @@ void SDL_Loader::deinit()
 {
 	IMG_Quit();
 	TTF_Quit();
+	Mix_Quit();
 	SDL_Quit();
 }
