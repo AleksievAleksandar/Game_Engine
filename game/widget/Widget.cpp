e738f63098b59b160e618e5f63cdef71c5d7419e
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-06-01 22:05:52 +0300

    fix setPrevFrame()

--- game/widget/Widget.cpp
+++ game/widget/Widget.cpp
@@ -38,7 +38,6 @@ void Widget::setNextFrame()
 	for (size_t i = 0; i < 6; i++)
 	{
 		std::cout << "x = " << frames.at(6).at(i).x << " y = " << frames.at(6).at(i).y << std::endl;
-		//std::cout << "w = " << it->second[i].w << " h = " << it->second[i].h << std::endl;
 	}
 
 	++this->_drawParams.currFrame;
@@ -47,50 +46,37 @@ void Widget::setNextFrame()
 		this->_drawParams.currFrame = 0;
 	}
 
-	//for (size_t i = 0; i < 6; i++)
-	//{
-	//	std::cout << "x = " << it->second[i].x << " y = " << it->second[i].y << std::endl;
-	//	//std::cout << "w = " << it->second[i].w << " h = " << it->second[i].h << std::endl;
-	//}
-
 	this->_drawParams.frame = frames.at(6).at(this->_drawParams.currFrame);
 	std::cout << "curr frame = " << this->_drawParams.currFrame << std::endl;
 	std::cout << "rsrc ID = " << this->_drawParams.rsrcId << std::endl;
-
-	//std::cout << "drawParam.frame.x = " << it->second[this->_drawParams.currFrame].x << std::endl;
-	//std::cout << "drawParam.frame.y = " << it->second[this->_drawParams.currFrame].y << std::endl;
-	//std::cout << "drawParam.frame.w = " << it->second[this->_drawParams.currFrame].w << std::endl;
-	//std::cout << "drawParam.frame.h = " << it->second[this->_drawParams.currFrame].h << std::endl;
-
-
-
-	//this->_drawParams.frame.x = 256;
 }
 
 void Widget::setPrevFrame()
 {
-	auto it = gRsrcMgr->getImageDimensions().find(this->_drawParams.rsrcId);
+	if (!this->_drawParams._hasFrames)
+	{
+		std::cerr << "Trying to set prev frame to image that has only one frame" << std::endl;
+		return;
+	}
 
-	//for (int32_t i = 0; i < 6; i++)
-	//{
-	//	std::cout << "x = " << it->second[i].x << " y = " << it->second[i].y << std::endl;
-	//	//std::cout << "w = " << it->second[i].w << " h = " << it->second[i].h << std::endl;
-	//}
+	std::unordered_map<int32_t, Frames> frames;
+	frames = gRsrcMgr->getImageDimensions();
+	frames.find(this->_drawParams.rsrcId);
+
+	for (size_t i = 0; i < 6; i++)
+	{
+		std::cout << "x = " << frames.at(6).at(i).x << " y = " << frames.at(6).at(i).y << std::endl;
+	}
 
 	--this->_drawParams.currFrame;
 	if (-1 == this->_drawParams.currFrame)
 	{
-		this->_drawParams.currFrame = 5;
+		this->_drawParams.currFrame = this->_drawParams.numFrames - 1;;
 	}
 
-	this->_drawParams.frame = it->second[this->_drawParams.currFrame];
+	this->_drawParams.frame = frames.at(6).at(this->_drawParams.currFrame);
 	std::cout << "curr frame = " << this->_drawParams.currFrame << std::endl;
 	std::cout << "rsrc ID = " << this->_drawParams.rsrcId << std::endl;
-
-	std::cout << "drawParam.frame.x = " << it->second[this->_drawParams.currFrame].x << std::endl;
-	std::cout << "drawParam.frame.y = " << it->second[this->_drawParams.currFrame].y << std::endl;
-	std::cout << "drawParam.frame.w = " << it->second[this->_drawParams.currFrame].w << std::endl;
-	std::cout << "drawParam.frame.h = " << it->second[this->_drawParams.currFrame].h << std::endl;
 }
 
 void Widget::setOpacity(const int32_t delta)
