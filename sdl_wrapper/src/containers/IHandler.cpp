a3eb515bab6ac4c7cd382a4a44de5e685dadeeea
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2022-06-08 18:18:06 +0300

    update collectSingleTextureForDrawing()

--- sdl_wrapper/src/containers/IHandler.cpp
+++ sdl_wrapper/src/containers/IHandler.cpp
@@ -22,7 +22,25 @@ void IHandler::collectSingleTextureForDrawing(SDL_Texture*& outCollection, const
 
 	if (it == this->_textures.end())
 	{
-		std::cerr << "ERROR -> IHandler::collectTexturesForDrawing() failed. Providing invalid rsrcId: " << drawParams.rsrcId << std::endl;
+		std::string widgetType;
+		switch (drawParams.widgetType)
+		{
+		case WidgetType::IMAGE:
+			widgetType = "IMAGE";
+			break;
+
+		case WidgetType::TEXT:
+			widgetType = "TEXT";
+			break;
+
+		default:
+			widgetType = "UNKNOWN";
+			break;
+		}
+
+		std::cerr << "ERROR -> IHandler::collectTexturesForDrawing() failed. Providing invalid rsrcId: " << drawParams.rsrcId 
+			      << " for Widget Type: " << widgetType << std::endl;
+
 		outCollection = nullptr;
 		return;
 	}
