f7cd30b536ab6836e3b8e2a72717c8d038965845
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2023-03-17 18:41:46 +0200

    fix indentation

--- cmake_helpers/helpers.cmake
+++ cmake_helpers/helpers.cmake
@@ -4,13 +4,13 @@ list(APPEND
     CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/cmake_helpers/find_modules)
     
 function(set_target_cpp_standard target standard)
-set_target_properties(
-    ${target}
-    PROPERTIES
-        CXX_STANDARD ${standard}
-        CXX_STANDARD_REQUIRED YES
-        CXX_EXTENSIONS NO
-)
+    set_target_properties(
+        ${target}
+        PROPERTIES
+            CXX_STANDARD ${standard}
+            CXX_STANDARD_REQUIRED YES
+            CXX_EXTENSIONS NO
+    )
 endfunction()
 
 function(enable_target_warnings target)
