871e89e56aa85cc9074a170ee58dd6ae937f8adc
Aleksandar Aleksiev <alexiev_alexander@abv.bg>
2023-05-12 18:35:41 +0300

    improve indentation

--- common_defines/Common_Defines.h
+++ common_defines/Common_Defines.h
@@ -18,13 +18,13 @@ namespace Textures
 
 	namespace ImageFrames
 	{
-		constexpr auto PRESS_KEYS_FRAMES = 1;
-		constexpr auto UP_FRAMES = 1;
-		constexpr auto DOWN_FRAMES = 1;
-		constexpr auto LEFT_FRAMES = 1;
-		constexpr auto RIGHT_FRAMES = 1;
-		constexpr auto LAYER_2_FRAMES = 1;
-		constexpr auto RUNNING_GIRL_FRAMES = 6;
+		constexpr auto PRESS_KEYS_FRAMES 	= 1;
+		constexpr auto UP_FRAMES 			= 1;
+		constexpr auto DOWN_FRAMES 			= 1;
+		constexpr auto LEFT_FRAMES 			= 1;
+		constexpr auto RIGHT_FRAMES 		= 1;
+		constexpr auto LAYER_2_FRAMES 		= 1;
+		constexpr auto RUNNING_GIRL_FRAMES 	= 6;
 	}
 }
 
